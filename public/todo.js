052a03be47af870fdbf0bcb92453747ad12daba1
Katia Romaniuk <katia-l@mail.ru>
2018-03-21 15:49:22 +0300

    added task service

--- public/todo.js
+++ public/todo.js
@@ -1,15 +1,27 @@
-angular.module('todoApp', [])
-  .controller('TodoListController', [ '$scope', function($scope) {
-    $scope.todos = [
-      {text:'learn AngularJS', done:true, date: 1521544682274},
-      {text:'learn JS core', done:true, date: 1511544682274},
-      {text:'build an AngularJS app', done:false, date: 1521544282274}
-    ];
+angular.module('todoApp', ['ui.router'])
+  .controller('TodoListController', [ '$scope', '$state', 'tasksService', function(
+    $scope,
+    $state,
+    tasksService
+  ) {
+    $scope.todos = tasksService.tasks;
 
     $scope.days = 1e100;
+    $scope.editIndex = -1;
 
     $scope.setDays = function (days) {
       $scope.days = days;
+    };
+
+    $scope.filterDone = function (task) {
+      return task.done;
+    };
+
+    $scope.editTask = function (date) {
+      $scope.editIndex = $scope.todos.reduce(function(acc, elem, index) {
+        return (elem.date === date) ? index : acc;
+      });
+      $scope.todoText = $scope.todos[$scope.editIndex].text;
     };
 
     $scope.orderTasks = function () {
@@ -27,8 +39,14 @@ angular.module('todoApp', [])
     $scope.addTodo = function() {
       $scope.validationError = $scope.todoText.length > 20;
       if ($scope.validationError) return;
-      $scope.todos.push({text:$scope.todoText, done:false, date: Date.now()});
+      if ($scope.editIndex > 0) {
+        $scope.todos[$scope.editIndex].text = $scope.todoText;
+        $scope.editIndex = -1;
+      } else {
+        $scope.todos.push({text:$scope.todoText, done:false, date: Date.now()});
+      }
       $scope.todoText = '';
+      $state.go('main');
     };
 
     $scope.remaining = function() {
@@ -46,4 +64,89 @@ angular.module('todoApp', [])
         if (!todo.done) $scope.todos.push(todo);
       });
     };
-  }]);
+  }])
+  .service('tasksService', [function() {
+     var tasks = [
+      {text:'learn AngularJS', done:true, date: 1521544682274},
+      {text:'learn JS core', done:true, date: 1511544682274},
+      {text:'build an AngularJS app', done:false, date: 1521544282274}
+    ];
+     return {
+       tasks: tasks
+     };
+   }])
+  .directive('main', function() {
+    return {
+      template: `<h2>Todo</h2>
+      <div ng-controller="TodoListController">
+        <span>{{remaining()}} of {{todos.length}} remaining</span>
+        [ <a href="" ng-click="archive()">archive</a> ]
+        <button ng-class="{active:days === 1}" ng-click="setDays(1)">1 day</button>&nbsp;
+        <button ng-class="{active:days === 3}" ng-click="setDays(3)">3 day</button>&nbsp;
+        <button ng-class="{active:days === 5}" ng-click="setDays(5)">5 day</button>&nbsp;
+        <button ng-class="{active:days === 1e100}" ng-click="setDays(1e100)">All dates</button>&nbsp;
+        <button ng-class="{active:sortTasksByName}" ng-click="orderTasks()">Sort by name</button>
+        <ul class="unstyled">
+          <li class="list-title">
+            Complited tasks
+          </li>
+          <li ng-repeat="todo in todos | filter:filterDays | orderBy:sortTasksByName | filter:filterDone">
+            <label class="checkbox">
+              <input type="checkbox" ng-model="todo.done">
+            </label>
+            <span class="done-{{todo.done}}" ng-click="editTask(todo.date)">{{todo.text}}</span>&nbsp;
+            <span class="task-date" ng-click="editTask(todo.date)">{{todo.date | date:'medium'}}</span>
+          </li>
+        </ul>
+        <ul class="unstyled">
+            <li class="list-title">
+              Uncomplited tasks
+            </li>
+          <li ng-repeat="todo in todos | filter:filterDays | orderBy:sortTasksByName | filter:!filterDone">
+              <label class="checkbox">
+                  <input type="checkbox" ng-model="todo.done">
+                </label>
+                <span class="done-{{todo.done}}" ng-click="editTask(todo.date)">{{todo.text}}</span>&nbsp;
+                <span class="task-date" ng-click="editTask(todo.date)">{{todo.date | date:'medium'}}</span>
+              </li>
+        </ul>
+        <a ui-sref="add" ui-sref-active="active">add task</a>
+  `
+    };
+  })
+  .directive('add', function() {
+    return {
+      template: `
+      <form name="myForm" ng-submit="addTodo()" ng-controller="TodoListController">
+        <input type="text" ng-model="todoText" name="input" size="30"
+              placeholder="add new todo here">
+        <input class="btn-primary" type="submit" value="add/change">
+        <span class="error" ng-show="todoText.length > 20">Length should be less 20!</span><br>
+      </form>
+  `
+    };
+  })
+  .config(function($stateProvider,$locationProvider) {
+    var mainState = {
+      name: 'main',
+      url: '/',
+      template: '<main></main>'
+    }
+  
+    var addState = {
+      name: 'add',
+      url: '/task/add',
+      template: '<add></add>'
+    }
+  
+    var editState = {
+      name: 'edit',
+      url: '/task/{taskId}/edit',
+      template: '<add></add>'
+    }
+  
+    $stateProvider.state(mainState);
+    $stateProvider.state(addState);
+    $stateProvider.state(editState);
+    $locationProvider.html5Mode(true);
+  });
--- server.js
+++ server.js
@@ -1,10 +1,28 @@
 var express = require('express')
 var path = require('path')
 var serveStatic = require('serve-static')
+var router = require('express').Router()
 
 var app = express()
 
 app.use(serveStatic(path.join(__dirname, 'node_modules')))
-app.use(serveStatic(path.join(__dirname, 'public')))
+router.all('*', function(req, res, next) {
+    // Just send the index.html for other files to support HTML5Mode
+    var match = req.url.match(/(\.js)|(\.css)|(\.woff)|(\.svg)|(\.png)|(\.map)|(\.ttf)|(\.\w+\?_etag)/i);
+    var file = (!match || req.url === '/') ? 'index.html' : req.url.slice(1);
+    var options = {
+        root: __dirname + '/public/',
+        dotfiles: 'deny',
+        headers: {
+            'x-timestamp': Date.now(),
+            'x-sent': true
+        }
+      };
+    
+    res.sendFile(file, options, function (err) {
+      if (err) next(err);
+    });
+});
+app.use(router)
 app.listen(3000)
-console.log('server started on port: 3000')
+console.log('server started on port: localhost:3000')
